59120f4c120162be2a717177211d7a803a5b9abd
T-Anh-k4 <146644118+T-Anh-k4@users.noreply.github.com>
2024-10-22 00:14:17 +0700

    new

--- DTO/KHO_DTO.cs
+++ DTO/KHO_DTO.cs
@@ -8,7 +8,15 @@ namespace DTO
 {
     public class KHO_DTO
     {
-        
+        public int IDKho { get; set; }
+        public string MAHH { get; set; }
+        public int soluong { get; set; }
         public KHO_DTO() { }
+        public KHO_DTO(int iDKho, string mAHH, int soluong)
+        {
+            IDKho = iDKho;
+            MAHH = mAHH;
+            this.soluong = soluong;
+        }
     }
 }
--- GUI/KHO_GUI.cs
+++ GUI/KHO_GUI.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BUS;
 
 namespace GUI
 {
     public partial class KHO_GUI : Form
     {
+        KHO_BUS kHOBUS;
         public KHO_GUI()
         {
+            kHOBUS = new KHO_BUS();
             InitializeComponent();
+            loadKho();
+        }
+        public void loadKho()
+        {
+            DataTable dt = new DataTable();
+            dt = kHOBUS.GetDanhSachHangHoa();
+            k_datagrview_hang_hoa.DataSource = dt;
         }
     }
 }
