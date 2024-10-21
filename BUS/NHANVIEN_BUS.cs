6b9b8657323d7877b8b03486c2747552ed860d8e
hoangvn03 <hoangnguyenngoc2k3@gmail.gmailcom>
2024-10-21 22:56:57 +0700

    upcode 21/10

--- BUS/NHANVIEN_BUS.cs
+++ BUS/NHANVIEN_BUS.cs
@@ -37,13 +37,13 @@ namespace BUS
 			return nhanVienDAO.GetGioiTinh();
 		}
 
-		public bool InsertNhanVien(string manv, string tennv, string gioitinh, string ngaysinh, string diachi, string sdt, string diengiai, int flag)
-		{
-			return nhanVienDAO.InsertNhanVien(manv, tennv, gioitinh, ngaysinh, diachi, sdt, diengiai, flag);
+        public bool InsertNhanVien(string maNV, string tenNV, string gioiTinh, string ngaySinh, string diaChi, string soDT, string dienGia, int flag, string username)
+        {
+            return nhanVienDAO.InsertNhanVien(maNV, tenNV, gioiTinh, ngaySinh, diaChi, soDT, dienGia, flag,username);
 		}
-		public bool UpdateNhanVien(string maNV, string tenNV, string gioiTinh, string ngaySinh, string diaChi, string soDT, string dienGia, int flag)
-		{
-			return nhanVienDAO.UpdateNhanVien(maNV, tenNV, gioiTinh, ngaySinh, diaChi, soDT, dienGia, flag);
+        public bool UpdateNhanVien(string maNV, string tenNV, string gioiTinh, string ngaySinh, string diaChi, string soDT, string dienGia, int flag, string username)
+        {
+            return nhanVienDAO.UpdateNhanVien(maNV, tenNV, gioiTinh, ngaySinh, diaChi, soDT, dienGia, flag, username);
 
 		}
 		public bool DeleteNhanVien(string maNV)
@@ -55,5 +55,9 @@ namespace BUS
 		{
 			return nhanVienDAO.SearchNhanVien(key);
 		}
-	}
+        public bool CheckUserName(string username)
+		{
+			return nhanVienDAO.CheckUserName(username);
+		}
+    }
 }
--- GUI/Program.cs
+++ GUI/Program.cs
@@ -17,7 +17,7 @@ namespace GUI
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			//Application.Run(new LOGIN_GUI());
-			Application.Run(new TEST_GUI());
+			Application.Run(new LOGIN_GUI());
 
 		}
 	}
