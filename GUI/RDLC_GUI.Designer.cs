dd459636cad164297608f440db34a93faee5616c
hoangvn03 <hoangnguyenngoc2k3@gmail.gmailcom>
2024-11-07 16:55:13 +0700

    up code

--- GUI/RDLC_GUI.Designer.cs
+++ GUI/RDLC_GUI.Designer.cs
@@ -29,93 +29,105 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
-            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.cTHDNDataSet = new GUI.CTHDNDataSet();
             this.cTHDNBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.cTHDNTableAdapter = new GUI.CTHDNDataSetTableAdapters.CTHDNTableAdapter();
-            this.cHITIETHDNHAPBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.cHITIET_HD_NHAPTableAdapter = new GUI.CTHDNDataSetTableAdapters.CHITIET_HD_NHAPTableAdapter();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.cTHDNDataSet)).BeginInit();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtSoHDN = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.cTHDNBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.cHITIETHDNHAPBindingSource)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // reportViewer1
             // 
-            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Top;
-            reportDataSource1.Name = "CTHDN";
-            reportDataSource1.Value = this.cTHDNBindingSource;
-            reportDataSource2.Name = "SHDN";
-            reportDataSource2.Value = this.cHITIETHDNHAPBindingSource;
-            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
-            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource2);
-            this.reportViewer1.LocalReport.ReportEmbeddedResource = "GUI.CTHDN_RP.rdlc";
-            this.reportViewer1.Location = new System.Drawing.Point(0, 100);
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reportViewer1.Location = new System.Drawing.Point(0, 142);
             this.reportViewer1.Name = "reportViewer1";
             this.reportViewer1.ServerReport.BearerToken = null;
-            this.reportViewer1.Size = new System.Drawing.Size(933, 471);
-            this.reportViewer1.TabIndex = 0;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(933, 100);
-            this.panel1.TabIndex = 1;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(78, 33);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(162, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // cTHDNDataSet
-            // 
-            this.cTHDNDataSet.DataSetName = "CTHDNDataSet";
-            this.cTHDNDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            this.reportViewer1.Size = new System.Drawing.Size(933, 647);
+            this.reportViewer1.TabIndex = 7;
             // 
             // cTHDNBindingSource
             // 
             this.cTHDNBindingSource.DataMember = "CTHDN";
-            this.cTHDNBindingSource.DataSource = this.cTHDNDataSet;
             // 
-            // cTHDNTableAdapter
+            // panel1
             // 
-            this.cTHDNTableAdapter.ClearBeforeFill = true;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(933, 142);
+            this.panel1.TabIndex = 6;
             // 
-            // cHITIETHDNHAPBindingSource
+            // panel3
             // 
-            this.cHITIETHDNHAPBindingSource.DataMember = "CHITIET_HD_NHAP";
-            this.cHITIETHDNHAPBindingSource.DataSource = this.cTHDNDataSet;
+            this.panel3.Controls.Add(this.txtSoHDN);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 51);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(933, 91);
+            this.panel3.TabIndex = 7;
             // 
-            // cHITIET_HD_NHAPTableAdapter
+            // txtSoHDN
             // 
-            this.cHITIET_HD_NHAPTableAdapter.ClearBeforeFill = true;
+            this.txtSoHDN.Location = new System.Drawing.Point(141, 33);
+            this.txtSoHDN.Name = "txtSoHDN";
+            this.txtSoHDN.Size = new System.Drawing.Size(121, 20);
+            this.txtSoHDN.TabIndex = 6;
+            this.txtSoHDN.TextChanged += new System.EventHandler(this.txtSoHDN_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Số hóa đơn nhập : ";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(933, 51);
+            this.panel2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(326, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(327, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Báo cáo chi tiết hóa đơn nhập";
             // 
             // RDLC_GUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(933, 597);
+            this.ClientSize = new System.Drawing.Size(933, 789);
             this.Controls.Add(this.reportViewer1);
             this.Controls.Add(this.panel1);
             this.Name = "RDLC_GUI";
             this.Text = "RDLC_GUI";
             this.Load += new System.EventHandler(this.RDLC_GUI_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.cTHDNDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.cTHDNBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.cHITIETHDNHAPBindingSource)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -123,12 +135,12 @@
         #endregion
 
         private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox textBox1;
-        private CTHDNDataSet cTHDNDataSet;
         private System.Windows.Forms.BindingSource cTHDNBindingSource;
-        private CTHDNDataSetTableAdapters.CTHDNTableAdapter cTHDNTableAdapter;
-        private System.Windows.Forms.BindingSource cHITIETHDNHAPBindingSource;
-        private CTHDNDataSetTableAdapters.CHITIET_HD_NHAPTableAdapter cHITIET_HD_NHAPTableAdapter;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtSoHDN;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
     }
 }
